2d9720d6ab1d753d6ec2087c736a4893b416a041
Olga Fadeeva <olafadeeva46791@mail.ru>
2025-02-14 18:32:14 +0300

    Temperature conversion task entered

---
+++ TypeOperationsLessons/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TypeOperationsLessons;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		//Конвертация температуры из цельсия в фаренгейт
+		/*
+		double C = double.Parse(Console.ReadLine());
+		double F = (C * 9 / 5 + 32);
+		Console.WriteLine("С: " + C + "\nF: " + F );
+		*/
+	}
+}
